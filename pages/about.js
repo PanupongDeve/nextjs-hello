797815365a641737c92076f491aab5a1b5ea67c4
PanupongDev <5635512110@psu.ac.th>
2018-11-27 20:40:52 +0700

    update compoennt

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import Layout from '../components/MyLayout.js'
+
+const about = (props) => {
+    return (
+        <Layout>
+          <p>This is the about page</p>
+        </Layout>
+      )
+}
+
+export default about;
